c092ef9937630690118796702d57fb305e29a4f1
adq8z9 <adq8z9@keemail.me>
2025-07-05 14:03:26 +0200

    Update index_script.js

--- src/index_script.js
+++ src/index_script.js
@@ -1,3 +1,19 @@
-function myFunction() {
-  document.getElementByID("mainText).innerHTML = "Paragraph changed.";
+function defaultOpen() {
+	document.getElementById("defaultOpen").click();
+}
+
+function openView(evt, oView) {
+  var mainDivs;
+  
+  mainDivs = document.getElementsByClassName("main");
+  for (i = 0; i < mainDivs.length; i++) {
+    mainDivs[i].style.display = "none";
+  }
+  menuPoints = document.getElementsByClassName("menu_point");
+  for (i = 0; i < menuPoints.length; i++) {
+    menuPoints[i].className = menuPoints[i].className.replace(" active", "");
+  }
+
+  document.getElementById(oView).style.display = "block";
+  evt.currentTarget.className += " active";
 }
