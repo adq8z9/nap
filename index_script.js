961a7001aae38a07eb4c49bf1f5d0a52023e03e1
adq8z9 <adq8z9@keemail.me>
2025-09-06 00:37:08 +0200

    Update index_script.js

--- index_script.js
+++ index_script.js
@@ -1,3 +1,10 @@
 function defaultOpen() {
   console.log("Start index");
+
+  let liKeypairString = localStorage.getItem("liKeypair");
+  if(liKeypairString !== null) {
+    let liKeypair = JSON.parse(liKeypairString);
+    let pk = NostrTools.nip19.npubEncode(liKeypair.pk);
+    document.getElementById("topNavLoginDataNpub").innerHTML = "npub: " + pk;
+  }
 }
