20eb7c3529de05ff5473218a92b4e2cf395ac288
adq8z9 <adq8z9@keemail.me>
2025-09-06 00:10:47 +0200

    Update login_script.js

--- login_script.js
+++ login_script.js
@@ -14,12 +14,12 @@ function createAndLogInNpub() {
     let pk = NostrTools.nip19.npubEncode(pkHex);
     console.log(pkHex);
     console.log(pk);
-    let keypair = { pk: pkhex, sk: skHex };
+    let keypair = { pk: pkHex, sk: skHex };
     localStorage.setItem("liKeypair", keypair); 
-    //document.getElementById("npubLoginInfo").innerHTML = "Currently logged in: " + NostrTools.nip19.npubEncode(localStorage.getItem("liKeypair"));
-    //document.getElementById("npub_right").innerHTML = NostrTools.nip19.npubEncode(localStorage.getItem("liPubkey"));
-    //let feedback = "Successfull key generation and log in.\nSecret key: " + NostrTools.nip19.npubEncode(localStorage.getItem("liKeypair");
-    //document.getElementById("npubCreateLoginInputFeedback").innerHTML = feedback;
+    document.getElementById("npubLoginInfo").innerHTML = "Currently logged in: " + pk;
+    document.getElementById("topNavLoginDataNpub").innerHTML = "npub: " + pk;
+    let feedback = "Successfull key generation and log in.\nSecret key: " + sk;
+    document.getElementById("npubCreateLoginInputFeedback").innerHTML = feedback;
   } catch (error) {
     let feedback = "Key generation failed: " + error;
     document.getElementById("npubCreateLoginInputFeedback").innerHTML = feedback;
