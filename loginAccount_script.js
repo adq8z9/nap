3652cccdf6028062d9db2ae340b7e51e704780a2
adq8z9 <adq8z9@keemail.me>
2025-09-06 01:44:37 +0200

    Update loginAccount_script.js

--- loginAccount_script.js
+++ loginAccount_script.js
@@ -6,60 +6,6 @@ function defaultOpen() {
     let liKeypair = JSON.parse(liKeypairString);
     let pk = NostrTools.nip19.npubEncode(liKeypair.pk);
     let sk = NostrTools.nip19.nsecEncode(NostrTools.utils.hexToBytes(liKeypair.sk));
-    document.getElementById("npubLoginInfo").innerHTML = "Currently logged in: " + pk;
     document.getElementById("topNavLoginDataNpub").innerHTML = "npub: " + pk;
-    document.getElementById("npubLoginInput").value = sk;
-  }
-}
-
-function logInNpub() {
-  let sk = document.getElementById("npubLoginInput").value;
-  try {
-    let skDec = NostrTools.nip19.decode(sk);
-    let skHex = NostrTools.utils.bytesToHex(skDec.data);
-    console.log(sk);
-    console.log(skDec);
-    console.log(skHex);
-    let pkHex = NostrTools.getPublicKey(skDec.data);
-    let pk = NostrTools.nip19.npubEncode(pkHex);
-    console.log(pkHex);
-    console.log(pk);
-    let keypair = { pk: pkHex, sk: skHex };
-    let keypairString = JSON.stringify(keypair);
-    localStorage.setItem("liKeypair", keypairString); 
-    document.getElementById("npubLoginInfo").innerHTML = "Currently logged in: " + pk;
-    document.getElementById("topNavLoginDataNpub").innerHTML = "npub: " + pk;
-    document.getElementById("npubLoginInput").value = sk;
-    let feedback = "Successfull log in.";
-    document.getElementById("npubLoginInputFeedback").innerHTML = feedback;
-  } catch (error) {
-    let feedback = "Log In failed. Nsec not in correct format. " + error;
-    document.getElementById("npubLoginInputFeedback").innerHTML = feedback;
-  }
-}
-
-function createAndLogInNpub() {
-  try {
-    let skDec = NostrTools.generateSecretKey();
-    let skHex = NostrTools.utils.bytesToHex(skDec);
-    let sk = NostrTools.nip19.nsecEncode(skDec);
-    console.log(skDec);
-    console.log(skHex);
-    console.log(sk);
-    let pkHex = NostrTools.getPublicKey(skDec);
-    let pk = NostrTools.nip19.npubEncode(pkHex);
-    console.log(pkHex);
-    console.log(pk);
-    let keypair = { pk: pkHex, sk: skHex };
-    let keypairString = JSON.stringify(keypair);
-    localStorage.setItem("liKeypair", keypairString); 
-    document.getElementById("npubLoginInfo").innerHTML = "Currently logged in: " + pk;
-    document.getElementById("topNavLoginDataNpub").innerHTML = "npub: " + pk;
-    document.getElementById("npubLoginInput").value = sk;
-    let feedback = "Successfull key generation and log in.<br>Public key: " + pk + "<br>Secret key: " + sk;
-    document.getElementById("npubCreateLoginInputFeedback").innerHTML = feedback;
-  } catch (error) {
-    let feedback = "Key generation failed: " + error;
-    document.getElementById("npubCreateLoginInputFeedback").innerHTML = feedback;
   }
 }
