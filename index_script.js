a2168d0811bdb209b58dc659cae42e8326c703f3
adq8z9 <adq8z9@keemail.me>
2025-09-11 21:51:07 +0200

    Update index_script.js

--- index_script.js
+++ index_script.js
@@ -7,4 +7,11 @@ function defaultOpen() {
     let pk = NostrTools.nip19.npubEncode(liKeypair.pk);
     document.getElementById("topNavLoginDataNpub").innerHTML = "npub: " + pk;
   }
+
+  let liLedgerString = localStorage.getItem("liLedger");
+  if(liLedgerString !== null) {
+    let liLedger = JSON.parse(liLedgerString);
+    let naddr = liLedger.naddr;
+    document.getElementById("topNavLoginDataLedger").innerHTML = "ledger: " + naddr;
+  }
 }
