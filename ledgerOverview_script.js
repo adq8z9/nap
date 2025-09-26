ff5a878c8bb0896a24b7ac29a98a4b2ce203ad6d
adq8z9 <adq8z9@keemail.me>
2025-09-26 14:49:00 +0200

    Update ledgerOverview_script.js

--- ledgerOverview_script.js
+++ ledgerOverview_script.js
@@ -9,7 +9,7 @@ function setLedgerViewTable() {
   if(liLedgerString !== null) {
     let liLedger = JSON.parse(liLedgerString);
     let ledgerEvent = liLedger.event;
-    document.getElementById("ledgerView").innerHTML = "Ledger: <br><br> Ledger Meta-Data";
+    document.getElementById("ledgerView").innerHTML = "Ledger Meta-Data";
     document.getElementById("ledgerViewTable").innerHTML = 
       "<tr><th>Account Category</th><th>Account ID</th><th>Account Name</th>" +
       "<tr><td>Income</td><td>acc_0001</td><td>Renumeration</td>";
