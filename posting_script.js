b3ba13976ec641e3b5846c754a5aa01cff5ade80
adq8z9 <adq8z9@keemail.me>
2025-10-04 16:09:41 +0200

    Update posting_script.js

--- posting_script.js
+++ posting_script.js
@@ -1,17 +1,4 @@
 function defaultOpen() {
   console.log("Start login");
-
-  let liKeypairString = localStorage.getItem("liKeypair");
-  if(liKeypairString !== null) {
-    let liKeypair = JSON.parse(liKeypairString);
-    let pk = NostrTools.nip19.npubEncode(liKeypair.pk);
-    document.getElementById("topNavLoginDataNpub").innerHTML = "npub: " + pk;
-  }
-
-  let liLedgerString = localStorage.getItem("liLedger");
-  if(liLedgerString !== null) {
-    let liLedger = JSON.parse(liLedgerString);
-    let naddr = liLedger.naddr;
-    document.getElementById("topNavLoginDataLedger").innerHTML = "ledger: " + naddr;
-  }
+  setLoginData();
 }
