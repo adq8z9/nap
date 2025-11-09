78a14c4fec68f2f03f2168a8f173c6db6dfaf94f
adq8z9 <adq8z9@keemail.me>
2025-11-09 16:13:02 +0100

    Refactor setLedgerViewTable for better clarity

--- ledgerOverview_script.js
+++ ledgerOverview_script.js
@@ -6,46 +6,66 @@ function defaultOpen() {
 
 async function setLedgerViewTable() {
   let liLedgerString = localStorage.getItem("liLedger");
-  if(liLedgerString !== null) {
-  try {
-    let liLedger = JSON.parse(liLedgerString);
-    let ledgerEventId = liLedger.id;
-    console.log(ledgerEventId);
-    let ledgerEvent = await getLedgerEventDB(ledgerEventId);
-    console.log(ledgerEvent);
-    let ledgerEventContent = JSON.parse(ledgerEvent.content);
-    console.log(ledgerEventContent);
-    let evLedgerAccounts = ledgerEventContent.acc_accounts;
-    let evLedgerAccountCategories = ledgerEventContent.acc_account_categories;
-    let evLedgerAccountants = ledgerEventContent.acc_accountants;
-    console.log(evLedgerAccounts);
-    console.log(evLedgerAccountCategories);
-    console.log(evLedgerAccountants);
-    let ledgerViewTableString = "<tr><th>Account ID</th><th>Account Name</th><th>Account Category</th></tr>";
-    for (let i = 0; i < evLedgerAccounts.length; i++) {
-      ledgerViewTableString += "<tr><td>" + evLedgerAccounts[i].id + "</td><td>" + evLedgerAccounts[i].name + "</td><td>";
-      let hasCategory = false;
-      for (let j = 0; j < evLedgerAccountCategories.length; j++) {
-        console.l
-        if (evLedgerAccounts[i].parent_id == evLedgerAccountCategories[j].id) {
-          ledgerViewTableString += evLedgerAccountCategories[j].name + "</td></tr>";
-          hasCategory = true;
+  let liKeypairString = localStorage.getItem("liKeypair");
+  if(liLedgerString !== null && liKeypairString !== null) {
+    try {
+      document.getElementById("ledgerView").innerHTML = "Loading.";
+      let liKeypair = JSON.parse(liKeypairString);
+      let liLedger = JSON.parse(liLedgerString);
+      let nAddrLedgerDec = NostrTools.nip19.decode(liLedger.naddr);
+      console.log(nAddrLedgerDec);
+      let ledgerEvent = await getLedgerEvent(nAddrLedgerDec, liKeypair.sk);
+      console.log(ledgerEvent);
+      let ledgerEventContent = JSON.parse(ledgerEvent.content);
+      console.log(ledgerEventContent);
+      let evLedgerAccounts = ledgerEventContent.acc_accounts;
+      let evLedgerAccountCategories = ledgerEventContent.acc_account_categories;
+      let evLedgerAccountants = ledgerEventContent.acc_accountants;
+      let evLedgerAccountantCategories = ledgerEventContent.acc_accountant_categories;
+      console.log(evLedgerAccounts);
+      console.log(evLedgerAccountCategories);
+      console.log(evLedgerAccountants);
+      console.log(evLedgerAccountantCategories);
+      let ledgerViewTableString = "<tr><th>Account Category</th><th>Account ID</th><th>Account Name</th></tr>";
+      for (let i = 0; i < evLedgerAccounts.length; i++) {
+        let hasCategory = false;
+        for (let j = 0; j < evLedgerAccountCategories.length; j++) {
+          if (evLedgerAccounts[i].parent_id == evLedgerAccountCategories[j].id) {
+            ledgerViewTableString += "<tr><td>" + evLedgerAccountCategories[j].name + "</td>";
+            hasCategory = true;
+          }
+        }
+        if (!hasCategory) {
+          ledgerViewTableString += "<tr><td>" + "- </td>";
+        }
+        ledgerViewTableString += "<td>" + evLedgerAccounts[i].id + "</td><td>" + evLedgerAccounts[i].name + "</td></tr>";
+      }
+      console.log(ledgerViewTableString);
+      let ledgerMetadataString = "Ledger Name: " + ledgerEventContent.name + "<br><br>Accountants: ";
+      for (let i = 0; i < evLedgerAccountants.length; i++) {
+        npubAcc = NostrTools.nip19.npubEncode(ledgerEventContent.acc_accountants[i].pubkey);
+        ledgerMetadataString += ledgerEventContent.acc_accountants[i].name + " (" + npubAcc.slice(0,8) + "..." + npubAcc.slice(-8);
+        let hasCategory = false;
+        for (let j = 0; j < evLedgerAccountantCategories.length; j++) {
+          if (evLedgerAccountants[i].parent_id == evLedgerAccountantCategories[j].id) {
+            ledgerMetadataString += ", " + evLedgerAccountantCategories[j].name + ") ";
+            hasCategory = true;
+          }
+        }
+        if (!hasCategory) {
+          ledgerMetadataString += ") ";
         }
       }
-      if (!hasCategory) {
-        ledgerViewTableString += " " + "</td></tr>";
-      }
-    }
-    console.log(ledgerViewTableString);
-    let ledgerMetadataString = "Name: " + ledgerEventContent.name + "<br><br>Units: " + ledgerEventContent.acc_units + "<br><br>Accountants: ";
-    for (let i = 0; i < evLedgerAccountants.length; i++) {
-      ledgerMetadataString += NostrTools.nip19.npubEncode(ledgerEventContent.acc_accountants[i].p) + " ";
-    }
-    console.log(ledgerMetadataString);
-    document.getElementById("ledgerView").innerHTML = ledgerMetadataString;
-    document.getElementById("ledgerViewTable").innerHTML = ledgerViewTableString;
-  } catch (error) {
-    document.getElementById("ledgerView").innerHTML = "Ledger loading failed: " + error;
-  } 
+      ledgerMetadataString += "<br><br>Accounting Units: " + ledgerEventContent.acc_units + "<br><br>Ledger accounts: <br>";
+      console.log(ledgerMetadataString);
+      document.getElementById("ledgerView").innerHTML = ledgerMetadataString;
+      document.getElementById("ledgerViewTable").innerHTML = ledgerViewTableString;
+    } catch (error) {
+      document.getElementById("ledgerView").innerHTML = "Ledger loading failed: " + error;
+    } 
+  } else if (liLedgerString == null) {
+    document.getElementById("ledgerView").innerHTML = "No Ledger selected.";
+  } else if (liKeypairString == null) {
+    document.getElementById("ledgerView").innerHTML = "No accountant logged in.";
   }
 }
