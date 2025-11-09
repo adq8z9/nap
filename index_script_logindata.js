403d88502babfeea6311785ca1a48187dd0807a1
adq8z9 <adq8z9@keemail.me>
2025-11-09 13:51:47 +0100

    Update index_script_logindata.js

--- index_script_logindata.js
+++ index_script_logindata.js
@@ -5,13 +5,13 @@ function setLoginData() {
   if(liKeypairString !== null && liLedgerString !== null) {
     let liKeypair = JSON.parse(liKeypairString);
     let liLedger = JSON.parse(liLedgerString);
-    document.getElementById("topNavLoginDataNpub").innerHTML = "accountant: " + liLedger.accountantName + " (" + liKeypair.npubShort + ")";
-    document.getElementById("topNavLoginDataLedger").innerHTML = "ledger: " + liLedger.ledgerName + " (" + liLedger.naddrShort + ")";
+    document.getElementById("topNavLoginDataNpub").innerHTML = "<b>accountant:</b> " + liLedger.accountantName + " (" + liKeypair.npubShort + ")";
+    document.getElementById("topNavLoginDataLedger").innerHTML = "<b>ledger:</b> " + liLedger.ledgerName + " (" + liLedger.naddrShort + ")";
   } else if(liKeypairString !== null) {
     let liKeypair = JSON.parse(liKeypairString);
-    document.getElementById("topNavLoginDataNpub").innerHTML = "accountant: " + liKeypair.npubShort;
+    document.getElementById("topNavLoginDataNpub").innerHTML = "<b>accountant:</b> " + liKeypair.npubShort;
   } else if(liLedgerString !== null) {
     let liLedger = JSON.parse(liLedgerString);
-    document.getElementById("topNavLoginDataLedger").innerHTML = "ledger: " + liLedger.ledgerName + " (" + liLedger.naddrShort + ")";
+    document.getElementById("topNavLoginDataLedger").innerHTML = "<b>ledger:</b> " + liLedger.ledgerName + " (" + liLedger.naddrShort + ")";
   }
 }
