adaed83c924b47655390d6e4603cb5e197ae516f
adq8z9 <adq8z9@keemail.me>
2025-11-15 16:37:59 +0100

    Add NWC wallet connection and login text box setup

--- postingNWC_script.js
+++ postingNWC_script.js
@@ -1,4 +1,24 @@
 function defaultOpen() {
   console.log("Start nwc posting");
   setLoginData();
+  setLoginTextBoxes();
+}
+
+function connectNWCWallet() {
+  console.log("Connect nwc Wallet");
+  try {
+    let nwcConnectionDataString = document.getElementById("connectNWCWalletInput").value;
+    console.log("" + nwcConnectionDataString);
+    let nwcConnectionData = NostrTools.nip47.parseConnectionString(nwcConnectionDataString);
+    console.log(nwcConnectionData);
+    console.log("Nwc wallet connected.");
+  } catch (error) {
+    console.log("Nwc wallet connection failed: " + error);
+    let feedback = "NWC wallet connection failed: " + error;
+    document.getElementById("connectNWCWalletInputFeedback").innerHTML = feedback;
+  }
+}
+
+function setLoginTextBoxes() {
+  console.log("LogInTextBoxes set.");
 }
