7597badba581f7298e30cdb8089055a84ac9e857
adq8z9 <adq8z9@keemail.me>
2025-11-15 17:17:32 +0100

    Update postingNWC_script.js

--- postingNWC_script.js
+++ postingNWC_script.js
@@ -6,16 +6,41 @@ function defaultOpen() {
 
 function connectNWCWallet() {
   console.log("Connect nwc Wallet");
-  try {
-    let nwcConnectionDataString = document.getElementById("connectNWCWalletInput").value;
-    console.log("" + nwcConnectionDataString);
-    let nwcConnectionData = NostrTools.nip47.parseConnectionString(nwcConnectionDataString);
-    console.log(nwcConnectionData);
-    console.log("Nwc wallet connected.");
-  } catch (error) {
-    console.log("Nwc wallet connection failed: " + error);
-    let feedback = "NWC wallet connection failed: " + error;
-    document.getElementById("connectNWCWalletInputFeedback").innerHTML = feedback;
+  let liLedgerString = localStorage.getItem("liLedger");
+  let liKeypairString = localStorage.getItem("liKeypair");
+  if(liLedgerString !== null && liKeypairString !== null) {
+    try {
+      let nwcConnectionDataString = document.getElementById("connectNWCWalletInput").value;
+      console.log("" + nwcConnectionDataString);
+      let nwcConnectionData = NostrTools.nip47.parseConnectionString(nwcConnectionDataString);
+      console.log(nwcConnectionData)
+      //Create data request
+      const nwcRequestData = {
+        tags: [
+          ["L", "leaccountingnip"],
+          ["p", "test"]
+        ], 
+        content: {
+          name: "test"
+        }   
+      };
+      let nwcRequest = NostrTools.finalizeEvent({
+        kind: 37701,
+        created_at: Math.floor(Date.now() / 1000),
+        tags: nwcRequestData.tags,
+        content: JSON.stringify(nwcRequestData.content),
+      }, nwcConnectionData.secret);
+      console.log(nwcRequest);
+      console.log("Nwc wallet connected.");
+    } catch (error) {
+      console.log("Nwc wallet connection failed: " + error);
+      let feedback = "NWC wallet connection failed: " + error;
+      document.getElementById("connectNWCWalletInputFeedback").innerHTML = feedback;
+    } 
+  } else if (liLedgerString == null) {
+      document.getElementById("connectNWCWalletInputFeedback").innerHTML = "Error: No Ledger selected.";
+  } else if (liKeypairString == null) {
+      document.getElementById("connectNWCWalletInputFeedback").innerHTML = "Error: No accountant logged in.";
   }
 }
 
