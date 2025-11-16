b6bd6a328ade8e09333e43ecc10f600e6b6b224a
adq8z9 <adq8z9@keemail.me>
2025-11-16 14:07:42 +0100

    Update index_script_ledger.js

--- index_script_ledger.js
+++ index_script_ledger.js
@@ -10,3 +10,16 @@ function getAccountantByPubkey(sPK, lEventContent) {
     throw "Ledger Event not in correct format.";
   }
 }
+
+function getIdentifier(tags) {
+  try {
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[i][0] == 'd') {
+        return tags[i][1];
+      }
+    }
+    return null;
+  } catch (error) {
+    throw "Ledger Event not in correct format.";
+  }
+}
